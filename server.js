d42893ceecce860abc661ef4147d7a24182208d0
dmatik <dmatik@gmail.com>
2021-07-13 11:19:09 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ var express = require('express'),
   app = express(),
   port = process.env.PORT || 3000;
 
-
 var routes = require('./api/routes/orefAlertsRoutes'); //importing route
 routes(app); 
 
